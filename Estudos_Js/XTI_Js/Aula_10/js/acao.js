2950ff909a242816228a5d8efeae2156e6f5e113
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-11-22 09:46:27 -0300

    estrutura de loop forin foreach

---
+++ Estudos_Js/XTI_Js/Aula_10/js/acao.js
@@ -0,0 +1,14 @@
+var numeros = new Array("um", "dois", "Três");
+
+//for (let i = 0; i < numeros.length; i++) {
+ //      alert(numeros[i]);
+//}
+
+//for(var i in numeros){
+  //  alert(numeros[i]);
+//}
+
+
+for( i in numeros){
+    alert(numeros[i]);
+}
---
+++ Estudos_Js/XTI_Js/Aula_11/js/acao.js
@@ -0,0 +1,11 @@
+var Carro = {
+  marca : "Nissan",
+  modelo : "350Z",
+  comprimeito : "3.345m",
+  velocidade : "320km/h",
+  cavalos : "350"
+};
+
+for( var i in Carro){
+    alert(i + " = "+  Carro[i]);
+}
