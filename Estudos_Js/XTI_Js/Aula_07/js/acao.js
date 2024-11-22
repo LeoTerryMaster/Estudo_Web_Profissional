62ee9a6ddd4b7512cc12abbfd940e2543f22a7fa
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-11-22 08:18:44 -0300

    estrutura de repeticao if&else

---
+++ Estudos_Js/XTI_Js/Aula_07/js/acao.js
@@ -0,0 +1,20 @@
+
+
+
+var idade = 60;
+
+if(idade>= 1 && idade<=2){
+alert("Temos um bebe de colo")
+}else if(idade>= 2 && idade<=7){
+    alert("Temos uma criança")
+}else if(idade>= 7 && idade<=13){
+    alert("Temos um pré adolescente")
+}else if(idade>= 13 && idade<=18){
+    alert("Temos um adolescente")
+}else if(idade>= 18 && idade<=60){
+    alert("Temos uma adulto")
+}else if(idade>= 60 && idade<=90){
+    alert("Temos um idoso")
+}else if(idade>= 60 && idade<=130){
+    alert("Temos um super idoso")
+}
