fda3505d3022fe2d7eaf52c17ab3a11eb266bc12
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-11-22 09:59:14 -0300

    while & dowhile

---
+++ Estudos_Js/XTI_Js/Aula_12/js/acao.js
@@ -0,0 +1,10 @@
+
+var i= 0;
+
+var texto = "";
+while(i<=10){
+    texto += i + " ";
+    i++;
+}
+
+alert(texto);
---
+++ Estudos_Js/XTI_Js/Aula_13/js/acao.js
@@ -0,0 +1,12 @@
+
+var i= 20;
+
+var texto = "";
+
+
+do{
+    texto += i + " ";
+    i++;
+}while(i<=10)
+
+alert(texto);
